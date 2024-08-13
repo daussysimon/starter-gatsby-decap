de4896d9b01dc346da2eb44f5dc95692bebce7e3
daussysimon <daussy.simon@gmail.com>
2024-08-12 20:03:38 -0700

    test

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,3 +1,13 @@
 import CMS from "decap-cms-app";
-import styles from "!css-loader!sass-loader!../../styles/admin.scss";
+import styles from "!css-loader!sass-loader!../styles/admin.scss";
+
 CMS.registerPreviewStyle(styles.toString(), { raw: true });
+
+const ArticlePreview = () => (
+  <article>
+    <h1>Custom article preview</h1>
+    <p>This is my new article.</p>
+  </article>
+);
+
+CMS.registerPreviewTemplate("home", ArticlePreview);
